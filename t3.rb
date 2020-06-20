5e0fc4dd194a01d7beee36ef2a87112489df15a0
Jose Hidago <jhidalgo521@gmail.com>
2020-06-20 16:09:05 +0000

    Automatically backed up by Learn

--- t3.rb
+++ t3.rb
@@ -1,9 +1,13 @@
+require 'pry'
+
+
 x = ["david", "robert", "jose"]
 
 def a(x)
   y = x.each_with_index do
     |n, i| "Hello, #{n}! You'll be assigned to room #{i}!"
   end
+  binding.pry
   y
 end
 
